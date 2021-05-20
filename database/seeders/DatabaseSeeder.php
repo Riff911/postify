39be2af1513890e4e5fbee77e2c90febb3a47e8c
unknown <safsonko1@gmail.com>
2021-05-20 23:01:33 +0000

    'changes'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::create([
+            'name' => 'Sheriff Sonko',
+            'email' => 'safsonko1@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('sheriff123'),
+            'remember_token' => Str::random(10),
+            'created_at' => $this->faker->dateTimeBetween('-6 month','+1 month')
+        ]);
         \App\Models\User::factory(50)->create();
+        
         // $this->call([
         //     PostSeeder::class,
         // ]);
